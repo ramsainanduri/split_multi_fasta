556138f6812111d39af61ba1d08c3460b8cd9652
ramsai7412 <ram.sai1@learner.manipal.edu>
2019-08-04 22:47:14 +0530

    Updated README.md added new feature in the main script to save the new files in the same directory as input file

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 Splitting one multi fasta file into multiple files with only one sequence each using the sequence IDs as file names.
 
 ## Usage
-
-python split_multi_fasta.py file_name.fasta/fa
-
+```bash
+To Run the script type in 'python split_multi_fasta.py /path/to/fasta/file'
+```
 ## Testing
 
-For testing purposes, there is file in test folder with name test.fasta which has 4 sequences. For your better understanding, you can use test.fasta to know how this tool works.
+For testing purpose, there is a file in test folder with name test.fasta which has 4 sequences. For your better understanding, you can use test.fasta to know how this tool works.
--- split_multi_fasta.py
+++ split_multi_fasta.py
@@ -2,12 +2,17 @@ import os
 import sys
 #Reading user inut file
 in_file = sys.argv[1]
-with open(in_file, "r") as fa:
-    lines=fa.read().split('>')
-    lines = lines[1:]
-    lines=['>'+ seq for seq in lines]
-    for name in lines:
-        file_name=name.split('\n')[0][1:]  #Extracting sequence Id to use it for file name
-        out_file=open(file_name+".fasta", "w")
-        out_file.write(name)
-        out_file.close()
+file_path = os.path.abspath(os.path.dirname(in_file))
+try:
+	with open(in_file, "r") as fa:
+	    lines=fa.read().split('>')
+	    lines = lines[1:]
+	    lines=['>'+ seq for seq in lines]
+	    for name in lines:
+	        file_name=name.split('\n')[0][1:]  #Extracting sequence Id to use it for file name
+	        out_file=open(file_path+"/"+file_name+".fasta", "w")
+	        out_file.write(name)
+	        out_file.close()
+	print ("\nSucessfully Split "+os.path.basename(in_file)+" into single fasta files")
+except:
+	sys.exit("Opps..either file is not present or file format is not correct")
